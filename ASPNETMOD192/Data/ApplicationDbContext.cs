d709610e8d4e7a2d9663973d5edb2c6747bd1dcd
Jorge Oliveira <Jorge Oliveira@DESKTOP-3K28CEO>
2024-11-03 18:11:25 +0000

    Add project files.

---
+++ ASPNETMOD192/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ASPNETMOD192.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMOD192.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client>Clients { get; set; } = default!;
+    }
+}
---
+++ ASPNETMOD192/Data/Migrations/20241025000526_CreateClientSchema.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPNETMOD192.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateClientSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthday = table.Column<DateOnly>(type: "date", nullable: false),
+                    Addresss = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    VatNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AdmissionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeactivationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CellPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
